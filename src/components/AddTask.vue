2ddff7cef176b8aa3afe8ac467e9bb9e147e3a03
Sherzod Xudoynazarov <loinnn1997@gmail.com>
2021-09-07 17:20:47 +0500

    add task  commet

---
+++ src/components/AddTask.vue
@@ -0,0 +1,22 @@
+<template>
+    <form class="add-form">
+        <div class="form-control">
+            <label>Task</label>
+            <input type="text" name="text" placeholder="Add task">
+        </div>
+        <div class="form-control">
+            <label>Day & Time</label>
+            <input type="text" name="day" placeholder="Add Day & Time">
+        </div>
+        <div class="form-control form-control-check">
+            <label>Reminder</label>
+            <input type="checkbox" name="reminder" value="Save Task">
+        </div>
+        <input type="submit" value="Save Task" class="btn btn-block">
+    </form>
+</template>
+<script>
+export default {
+    name: "AddTask",
+}
+</script>
